a043a9974ad3dc6e10541a049316254d561cce8d
Matt Lewis <mlewis@c3metrics.com>
2017-10-29 16:24:26 -0400

    Get and display access tokens

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const exphbs = require('express-handlebars');
 const app = express();
 const OAuth2 = google.auth.OAuth2;
 
+// Initialize the client, it will do all the OAauth2 requests for us
 const oauth2Client = new OAuth2(
   'CLIENT_ID',
   'CLIENT_SECRET',
   'REDIRECT_URL'
 );
 
+// We direct useres to authenticate through this url
 const authUrl = oauth2Client.generateAuthUrl({
   access_type: 'online',
   scope: [
@@ -20,6 +22,7 @@ const authUrl = oauth2Client.generateAuthUrl({
   state: 'puppy'
 });
 
+// Globals are gross, but this is a 48 line example app
 let accessTokens = [];
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -28,19 +31,26 @@ app.set('view engine', 'handlebars');
 app.get('/', (req, res) => (
   res.render('home', {
     authUrl: authUrl,
-    test: 'poop'
+    accessTokens: accessTokens.length ? accessTokens : false
   })
 ));
 
 app.get('/oauthcallback', (req, res) => (
-  console.log(req)
+  oauth2Client.getToken(req.query.code, (err, tokens) => {
+    if (!err) {
+      accessTokens = [ tokens.access_token, ...accessTokens];
+    }
+    res.location('/');
+
+    // Render the page again, we keep the query in the header so
+    // people can see the entire auth code url
+    return  res.render('home', {
+      authUrl: authUrl,
+      accessTokens: accessTokens.length ? accessTokens : false
+    });
+  })
 ));
 
 app.listen(3000, () => (
   console.log('The puppies are listening on port 3000!')
 ));
-
-
-// Wrapper around making the auth url
-const makeAuthUrl = () => {
-};
